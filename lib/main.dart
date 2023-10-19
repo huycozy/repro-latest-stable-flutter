eef588f1952eae1271ccd8f9f845fd2c8bea760e
Nguyen Huy <huy@nevercode.io>
2023-10-19 18:51:01 +0700

    2nd: 2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.ac_unit),
+              Icon(Icons.accessibility),
             ],
           ),
         ),
